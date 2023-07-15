558fbaaf36c1b73f94254095c938582df974f635
har <yinchun.har@gmail.com>
2023-07-15 14:59:18 +0900

    up

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
   font-family Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 70px
+  color #000
+  margin-top 10px
 </style>
